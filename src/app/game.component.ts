737aa5c4c5c4a58f59ac06df8b1073b4500674dc
bigodines <bigodines@gmail.com>
2017-03-31 16:39:54 -0700

    wip. broken

---
+++ src/app/game.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * Tetris screen component
+ * */
+
+@Component({
+  selector: 'game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class GameComponent {
+  title = 'Blockboom!';
+}
