675cf55bf1b2ef63edfffeccf6250e24442ce8b0
matheus mendes <bigodines@gmail.com>
2017-04-03 22:39:40 -0700

    Plot twist (#1)
    * changing my mind.
    
    * progress is made. time to print
    
    * I can draw stuff on the canvas! yay!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game.component';
+
+import { GameService } from './game.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ GameService ],
+  bootstrap: [AppComponent, GameComponent]
 })
 export class AppModule { }
---
+++ src/app/game.component.ts
@@ -0,0 +1,98 @@
+import {
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild
+} from '@angular/core';
+
+import { GameService } from './game.service';
+
+enum Types {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G
+}
+/**
+ *  Game screen component
+ * */
+@Component({
+    selector: 'game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class GameComponent implements OnInit {
+
+    protected ctx:CanvasRenderingContext2D;
+    protected grid:Array<Array<String>> = [];
+    protected numrows = 10
+    protected numlines = 10;
+
+    @ViewChild("canvas") canvas: ElementRef;
+
+    constructor(private service: GameService) {
+
+     }
+
+    ngOnInit() {
+        console.log(this.canvas);
+        this.ctx = this.canvas.nativeElement.getContext("2d");
+        this.buildSprite();
+        this.buildBoard();
+        this.draw();
+    }
+
+    /** does nothing for now. */
+    buildSprite() {
+        return;
+    }
+
+    buildBoard() {
+        let x:number;
+        let y:number;
+        let row:Array<any>;
+        let rand:number;
+
+        for (x = 0; x < this.numrows; x++) {
+            row = [];
+            for (y = 0; y < this.numlines; y++) {
+                row.push(this._getRandomEnum<Types>(Types));
+            }
+            this.grid.push(row);
+        }
+    }
+
+    draw() {
+        let x:number;
+        let y:number;
+        let row:Array<String>;
+
+        for(x = 0; x < this.grid.length; x++) {
+            row = this.grid[x];
+            for(y = 0; y < row.length; y++) {
+                this.print(x, y, row[x][y]);
+            }
+        }
+
+    }
+
+    print(x:number, y:number, block:String) {
+        // TODO: Print one particular element.
+        console.log(x,y);
+        this.ctx.fillStyle = 'blue';
+        this.ctx.fillRect(1+(x * 20), 1+(y * 10), 10, 3);
+    }
+
+
+    /** helper candidates */
+    /** Get a random element  from ENUM */
+    _getRandomEnum<E>(e: any): String {
+        var keys = Object.keys(e),
+            index = Math.floor(Math.random() * keys.length),
+            k = keys[index];
+        return k;
+    }
+}
