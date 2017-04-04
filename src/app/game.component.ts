f3d1391769d604b29f2b3a57f2cc8cbce9c181b4
bigodines <bigodines@gmail.com>
2017-04-03 22:08:24 -0700

    progress is made. time to print

--- src/app/game.component.ts
+++ src/app/game.component.ts
@@ -1,34 +1,87 @@
-import { 
+import {
     Component,
     OnInit
 } from '@angular/core';
 
 import { GameService } from './game.service';
 
+enum Types {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G
+}
 /**
  *  Game screen component
  * */
 @Component({
-  selector: 'game',
-  templateUrl: './game.component.html',
-  styleUrls: [ './app.component.css' ]
+    selector: 'game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class GameComponent implements OnInit {
-  
-  constructor(private service: GameService) { }
 
-  ngOnInit() {
-      this.buildSprite();
-      this.buildBoard();
-      console.log(this.service);
-  }
+    protected grid:Array<Array<String>> = [];
+    protected numrows = 10
+    protected numlines = 10;
 
-  /** does nothing for now. */
-  buildSprite() {
-      return;
-  }
+    constructor(private service: GameService) { }
 
-  buildBoard() {
+    ngOnInit() {
+        this.buildSprite();
+        this.buildBoard();
+        this.draw();
+        console.log(this);
+    }
 
-  }
+    /** does nothing for now. */
+    buildSprite() {
+        return;
+    }
+
+    buildBoard() {
+        let x:number;
+        let y:number;
+        let row:Array<any>;
+        let rand:number;
+
+        for (x = 0; x < this.numrows; x++) {
+            row = [];
+            for (y = 0; y < this.numlines; y++) {
+                row.push(this._getRandomEnum<Types>(Types));
+            }
+            this.grid.push(row);
+        }
+    }
+
+    draw() {
+        let x:number;
+        let y:number;
+        let row:Array<String>;
+
+        for(x = 0; x < this.grid.length; x++) {
+            row = this.grid[x];
+            for(y = 0; y < row.length; y++) {
+                this.print(x, y, row[x][y]);
+            }
+        }
+
+    }
+
+    print(x:number, y:number, block:String) {
+        // TODO: Print one particular element.
+    }
+
+
+    /** helper candidates */
+    /** Get a random element  from ENUM */
+    _getRandomEnum<E>(e: any): String {
+        var keys = Object.keys(e),
+            index = Math.floor(Math.random() * keys.length),
+            k = keys[index];
+        return k;
+    }
 }
